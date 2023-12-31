a643a300961c076a8466e87e8d702446c03bb978
Hye-joon Jeon <137747849+hyejoony@users.noreply.github.com>
2023-12-31 22:36:32 +0900

    Update underwater_1.py

--- streamlit/page/underwater_1.py
+++ streamlit/page/underwater_1.py
@@ -17,15 +17,19 @@ from datetime import datetime, timedelta
 import geemap.foliumap as eefolium  # eefolium 버전 사용 
 
 import change, parks
-import matplotlib
 import matplotlib.font_manager as fm
+import os
 
 def app():
 
-    font_path = './streamlit/font/malgun.ttf'
-    # 폰트 프로퍼티 설정
+    # font_path = './streamlit/font/malgun.ttf'
+    # # 폰트 프로퍼티 설정
+    # font_prop = fm.FontProperties(fname=font_path, size=12)
+    # # matplotlib의 폰트를 설정
+    # plt.rcParams['font.family'] = font_prop.get_name()
+    
+    font_path = os.path.abspath('./streamlit/font/malgun.ttf')
     font_prop = fm.FontProperties(fname=font_path, size=12)
-    # matplotlib의 폰트를 설정
     plt.rcParams['font.family'] = font_prop.get_name()
 
     # 페이지 제목 설정

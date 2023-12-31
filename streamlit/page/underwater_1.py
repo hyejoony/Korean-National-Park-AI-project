de739ea2560383da2de5af1b02ce8d8daf879432
Hye-joon Jeon <137747849+hyejoony@users.noreply.github.com>
2023-12-31 23:41:25 +0900

    Update underwater_1.py

--- streamlit/page/underwater_1.py
+++ streamlit/page/underwater_1.py
@@ -20,7 +20,7 @@ import change, parks
 import matplotlib.font_manager as fm
 
 def app():
-    font_path = 'main/streamlit/font/malgun.ttf'
+    font_path = './streamlit/font/malgun.ttf'
     # 폰트 프로퍼티 설정
     font_prop = fm.FontProperties(fname=font_path, size=12)
     # matplotlib의 폰트를 설정
